39005b81532061eddf4ef0044dc3c838f167af11
elenamorton <elena.uleia@gmail.com>
2017-07-11 13:44:14 +0100

    added printing one line of teh statement

--- lib/printer.rb
+++ lib/printer.rb
@@ -2,10 +2,25 @@ class Printer
 
   HEADERS = "date || credit || debit || balance"
 
+  def initialize(transactions = Transactions.new)
+    @transactions = transactions
+  end
 
+
+  def statement
+
+  end
+
+#private
 
   def headers
     HEADERS
+  end
+
+  def statementLine(transaction)
+    return "#{transaction[:date]} || || #{transaction[:amount]} || " if transaction[:withdraw]
+    return "#{transaction[:date]} || #{transaction[:amount]} || || " if transaction[:deposit]
+    raise "Transaction type error!"
   end
 
 end
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -2,6 +2,7 @@ require 'printer'
 
 describe Printer do
   subject(:printer) { described_class.new }
+  let(:transactions) { double :transactions, log: [{ date: "10/01/2012", deposit: 20 }, { date: "14/01/2012", withdraw: 10 }] }
 
   describe '#headers' do
     it 'can print the statement headers' do
@@ -9,6 +10,12 @@ describe Printer do
     end
   end
 
-
+  describe '#statement' do
+    it 'can print a statement line' do
+      now = Time.parse("2012-01-10 20:17:40")
+      allow(Time).to receive(:now) { now }
+      expect(printer.statementLine(transactions.log.first)).to include now.strftime("%d/%m/%Y")
+    end
+  end
 
 end
