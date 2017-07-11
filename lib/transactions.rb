ea0da23feb69130e714292d5dbe81c8357026b9b
elenamorton <elena.uleia@gmail.com>
2017-07-11 11:43:04 +0100

    adding deposit operation and save it to transactions log

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -1,3 +1,22 @@
 class Transactions
 
+  attr_reader :log
+
+  def initialize
+    @log = []
+    @transaction = Hash.new { 0 }
+  end
+
+  def deposit(amount)
+    @transaction[:deposit] = amount
+    @transaction[:date] = Time.now.strftime("%d/%m/%Y")
+    record_transaction(@transaction)
+  end
+
+
+private
+  def record_transaction(operation)
+    log.unshift(operation)
+  end
+
 end
--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -2,8 +2,18 @@ require 'transactions'
 
 
 describe Transactions do
+  subject(:transactions) { described_class.new }
 
+  it "expects account to have an empty log at start" do
+      expect(transactions.log).to eq []
+  end
 
+  describe '#deposit' do
+    it 'can make a deposit' do
+      transactions.deposit(20)
+      expect(transactions.log.first.values).to include 20
+    end
 
-  
+  end
+
 end
