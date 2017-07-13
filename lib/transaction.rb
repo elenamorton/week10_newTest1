8947b8591773628ccd08b9bbd497c6e59d6ec727
elenamorton <elena.uleia@gmail.com>
2017-07-13 21:55:29 +0100

    refactoring transaction calss and TDD.

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,3 +1,18 @@
 class Transaction
 
+  attr_reader :transaction
+
+  def initialize
+    @transaction = Hash.new { 0 }
+  end
+
+  def deposit(amount)
+    @transaction[:deposit] = amount
+  end
+
+  def withdraw(amount)
+    @transaction[:withdraw] = amount
+  end
+
+
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,31 +1,22 @@
 require 'transaction'
 
 describe Transaction do
-  subject(:transaction) { described_class.new }
+  subject(:bank_operation) { described_class.new }
 
   describe '#deposit' do
     it 'can make a deposit' do
-      transaction.deposit(20)
-      expect(transaction[:deposit]).to eq 20
-      expect(transaction[:withdraw]).to eq 0
+      bank_operation.deposit(20)
+      expect(bank_operation.transaction[:deposit]).to eq 20
+      expect(bank_operation.transaction[:withdraw]).to eq 0
     end
   end
 
   describe '#withdraw' do
     it 'can make a withdrawl' do
-      transaction.withdraw(10)
-      expect(transaction[:withdraw]).to eq 10
-      expect(transaction[:deposit]).to eq 0
+      bank_operation.withdraw(10)
+      expect(bank_operation.transaction[:withdraw]).to eq 10
+      expect(bank_operation.transaction[:deposit]).to eq 0
     end
   end
-
-  describe '#overdraft' do
-    it 'can check if current transaction is overdraft' do
-      transaction.deposit(20)
-      transaction.withdraw(30)
-      expect(transaction.overdraft?).to be(true)
-    end
-  end
-
 
 end
